c41ea8e3d3b966da182332f1367be59a924f4ad4
Ankit <Ankit.sinha@esyasoft.com>
2024-10-14 16:34:22 +0530

    commits

--- controllers/chargerLocationController.js
+++ controllers/chargerLocationController.js
@@ -78,7 +78,7 @@ const getAllChargers = async (req, res) => {
             return res.status(401).json({ success: false, message: "You are Not a Valid User." });
         }
         // Find all ChargerLocation documents but only return the chargerInfo field
-        const chargers = await ChargerLocation.find({}, 'chargerInfo locationName city state');
+        const chargers = await ChargerLocation.find({}, 'chargerInfo locationType locationName city state');
 
         if (chargers.length === 0) {
             return res.json({
