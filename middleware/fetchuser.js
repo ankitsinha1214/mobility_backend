530e552ba329070a0293b3684a3d2fdfe5a30971
Ankit <Ankit.sinha@esyasoft.com>
2025-01-28 15:05:55 +0530

    updarts

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
+const User = require('../models/userModel');
 
-const fetchUser = (req, res, next) => {
+const fetchUser = async(req, res, next) => {
     // Get the token from the authorization header
     const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
-        return res.status(401).json({success: false, message: "Token Not Found!" });
+        return res.status(401).json({ success: false, message: "Token Not Found!" });
     }
 
     try {
@@ -15,10 +16,17 @@ const fetchUser = (req, res, next) => {
         req.user = data.role;
         req.userid = data._id;
         req.phn = data.phoneNumber;
+        if (data.phoneNumber) {
+            // Optional: Verify if the user exists in the database (based on the phoneNumber or ID)
+            const user = await User.findOne({ phoneNumber: data.phoneNumber });
+            if (!user) {
+                return res.status(401).json({ success: false, message: "User not found or Inactive." });
+            }
+        }
         // console.log(req);
         next();
     } catch (error) {
-        return res.status(401).json({success:false, message: "Please Authenticate using a Valid Token." });
+        return res.status(401).json({ success: false, message: "Please Authenticate using a Valid Token." });
     }
 };
 
