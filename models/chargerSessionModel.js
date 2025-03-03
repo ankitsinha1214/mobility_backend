cefba3eb51055a4adf94caba58ed4c174b73a012
Ankit <Ankit.sinha@esyasoft.com>
2025-03-03 12:22:16 +0530

    updates

--- models/chargerSessionModel.js
+++ models/chargerSessionModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ChargerLocation = require('./chargerLocationModel');
 
 const chargingSessionSchema = new Schema({
     chargerId: {
@@ -72,9 +73,32 @@ const chargingSessionSchema = new Schema({
 }, { timestamps: true });
 
 // Middleware to remove transactionId when status is updated to 'Completed'
-chargingSessionSchema.pre('save', function (next) {
+chargingSessionSchema.pre('save', async function (next) {
     if (this.isModified('status') && this.status === 'Completed') {
         this.transactionId = undefined; // Remove transactionId
+    }
+    if (this.isModified('status') && this.endMeterValue !== undefined && this.startMeterValue !== undefined) {
+        const energyConsumed = (this.endMeterValue - this.startMeterValue)/1000;
+
+        try {
+            // const chargerLocation = await ChargerLocation.findOne({ 'chargerInfo.name': this.chargerId });
+            const chargerLocation = await ChargerLocation.findOne({
+                chargerInfo: { $elemMatch: { name: this.chargerId } }
+            });
+
+            if (chargerLocation) {
+                const charger = chargerLocation.chargerInfo.find(charger => charger.name === this.chargerId);
+
+                if (charger) {
+                    const currentEnergy = parseFloat(charger.energyConsumptions.replace(' kWh', '')) || 0;
+                    charger.energyConsumptions = `${(currentEnergy + energyConsumed).toFixed(2)} kWh`;
+
+                    await chargerLocation.save();
+                }
+            }
+        } catch (error) {
+            console.error('Error updating charger energy consumption:', error);
+        }
     }
     next();
 });
