f5a8ea8326400db5c8e708ce541b17e8a3806881
Ankit <Ankit.sinha@esyasoft.com>
2025-05-23 19:01:48 +0530

    updates

--- models/logModel.js
+++ models/logModel.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose');
 
 const chargerLogSchema = new mongoose.Schema({
     type: { type: String, enum: ["Charger", "User", "Others"], default: "Charger" },
-  chargerId: {
-    type: String,
-    required: true
-  },
-  action: {
-    type: String,
-    required: true
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  },
-  message: {
-    type: mongoose.Schema.Types.Mixed,
-    required: true
-  }
+    chargerId: {
+        type: String,
+        required: true
+    },
+    action: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    message: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true
+    }
 }, { timestamps: true });
 
-module.exports = mongoose.model('ChargerLog', chargerLogSchema);
+module.exports = mongoose.model('Logs', chargerLogSchema);
