e23be396b5970473713a8ab45b2e0001e6b09328
Ankit <Ankit.sinha@esyasoft.com>
2025-05-28 15:24:27 +0530

    updates

--- models/userModel.js
+++ models/userModel.js
@@ -162,7 +162,7 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
-    required: true,
+    // required: true,
     // unique: true,
     validate: {
       validator: function(v) {
